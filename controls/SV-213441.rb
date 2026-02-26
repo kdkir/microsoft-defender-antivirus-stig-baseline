eeefb085c60fc3c228f5a5b5766dd35be45ab406
kdkir <kekirwin@gmail.com>
2026-02-25 21:25:55 -0500

    Implement control check for SV-213441

--- controls/SV-213441.rb
+++ controls/SV-213441.rb
@@ -24,4 +24,14 @@ If the value is 1, this is a finding.'
   tag legacy: ['SV-89899', 'V-75219']
   tag cci: ['CCI-001242']
   tag nist: ['SI-3 c 1']
+
+  registry_path = 'HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection'
+
+  describe registry_key(registry_path) do
+    # Missing value => nil => passes
+    # Value = 0 => Passes
+    # Value = 1 => FAILS
+    its('LocalSettingOverrideDisableRealtimeMonitoring') { should eq 0 }
+  end
+
 end
