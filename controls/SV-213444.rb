395188a00e3d9e63f1f2be9c0a7d779b28431578
kdkir <kekirwin@gmail.com>
2026-02-25 21:30:02 -0500

    Implement control check for SV-213444

--- controls/SV-213444.rb
+++ controls/SV-213444.rb
@@ -24,4 +24,14 @@ If the value is 1, this is a finding.'
   tag legacy: ['SV-89905', 'V-75225']
   tag cci: ['CCI-001169']
   tag nist: ['SC-18 (3)']
+
+  registry_path = 'HKLM\\Software\\Policies\\Microsoft\\Windows Defender\\Real-Time Protection'
+
+  describe registry_key(registry_path) do
+    # Missing value => nil => passes
+    # Value = 0 => Passes
+    # Value = 1 => FAILS
+    its('DisableIOAVProtection') { should eq 0 }
+  end
+
 end
